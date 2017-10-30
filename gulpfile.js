c476b8126166a966e8d9ee6e757f040a64919ef6
cherogit <chernenkoan91@gmail.com>
2017-10-30 13:43:13 +0300

    first upload

---
+++ gulpfile.js
@@ -0,0 +1,138 @@
+'use strict'
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const livereload = require('gulp-livereload');
+const del = require('del');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested');
+const short = require('postcss-short');
+const assets = require('postcss-assets');
+const Import = require('postcss-import');
+const cssnext = require('cssnext');
+const mqpacker = require("css-mqpacker");
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const cssnano = require('cssnano');
+const notify = require('gulp-notify');
+const sourcemaps = require('gulp-sourcemaps');
+const pug = require('gulp-pug');
+
+// Clean =================================
+gulp.task('clean', function() { 
+	return del('build/*');
+});
+
+// Pages =================================
+gulp.task('pug', function() {
+	return gulp.src('./src/pug/*.pug')
+		.pipe(pug({pretty: true}))  //с переносом pretty: true
+		.pipe(gulp.dest('build'))
+});
+
+// Css =====================================
+
+gulp.task('styles:vendor', function() {
+    return gulp.src('./src/styles/vendorCss/*.css')
+        .pipe(gulp.dest('./build/css/vendorCss/'))
+});
+
+gulp.task('styles', function () {
+    var processors = [
+    	Import,
+        autoprefixer({browsers: ['last 3 version']}),
+    	cssnext({
+    		'customProperties': true,
+    		'customFunction': true,
+    		'customSelectors': true,
+    	}),
+    	//cssnano(),
+    	nested,
+    	short,
+    	assets({
+    		loadPaths: ['src/assets/fonts/**/*', 'src/assets/images/**/*'],
+    		relativeTo: 'src/styles/'
+    	}),
+        mqpacker({
+            sort: true
+          })
+    ];
+    return gulp.src('./src/styles/style.css')
+        .pipe(postcss(processors))
+        .pipe(rename('styles.css')) // как называется новый файл
+        .pipe(gulp.dest('./build/css/')) // куда его положить
+});
+
+// Scripts =================================
+gulp.task('js', function() {
+    return gulp.src('./src/scripts/**/*')
+    	// .pipe(concat('min.js'))
+		.pipe(gulp.dest('build/js/'))
+});
+
+// Fonts =================================
+gulp.task('fonts', function() {
+	return gulp.src('./src/assets/fonts/**/*')
+	    .pipe(gulp.dest('build/assets/fonts/'))
+});
+
+// Source =================================
+gulp.task('source', function() {
+    return gulp.src('./src/source/**/*')
+        .pipe(gulp.dest('build/source/'))
+});
+
+// Images =================================
+gulp.task('img', function() {
+	return gulp.src('./src/assets/images/**/*')
+		// .pipe(imagemin())
+		.pipe(gulp.dest('build/assets/images/'))
+});
+
+// Watch =================================
+gulp.task('watch', function() {
+	gulp.watch('src/pug/**/*.pug', gulp.series('pug'));
+    gulp.watch('src/styles/**/*.css', gulp.series('styles'));
+	gulp.watch('src/styles/vendorCss/*.css', gulp.series('styles:vendor'));
+	gulp.watch('src/scripts/**/*.js', gulp.series('js'));
+	gulp.watch('src/assets/fonts/**/*', gulp.series('fonts'));
+    gulp.watch('src/assets/images/**/*', gulp.series('img'));
+	gulp.watch('src/source/**/*', gulp.series('source'));
+});
+
+gulp.task('watcher', function() {
+    var watcher = gulp.watch('src/pug/**/*.pug', gulp.series('pug'));
+    var timeout = setTimeout(watcher.end, 60*60*1000);
+
+    watcher.on('change', function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(watcher.end, 60*60*1000);
+    });
+});
+
+// Build =================================
+gulp.task('build', gulp.series(
+    'clean', 
+    gulp.parallel('pug', 'styles', 'styles:vendor', 'js', 'fonts', 'img', 'source'))
+);
+
+// Server =================================
+gulp.task('serve', function() {
+    browserSync.init({
+        injectChanges: true,
+        server: './build'
+    });
+
+    browserSync.watch('build').on('change', browserSync.reload);
+})
+
+gulp.task('dev', 
+	gulp.series('build', gulp.parallel('watch', 'serve'))
+);
+
+// Default ----------------------------------------
+gulp.task('default',
+    gulp.series(gulp.parallel('watch', 'serve'))
+);
---
+++ src/scripts/custom/script.js
@@ -0,0 +1,103 @@
+$(function() {
+	
+	$('#fullpage').fullpage({
+		menu: '#myMenu',
+		anchors:[
+			'firstPage', 
+			'secondPage', 
+			'thirdPage', 
+			'fourthPage', 
+			'fifthPage', 
+			'sixthPage', 
+			'seventhPage'
+		],
+		sectionsColor: [
+			'#4fbbc3', 
+			'#efe4a9', 
+			'#de806a', 
+			'#ebe3e3',
+			'#7abac1',
+			'#3b919b',
+			'#4f404d'
+		],
+		scrollingSpeed: 600,
+	});
+
+	$('.burger-btn').click(function() {
+		$(this).toggleClass('_active');
+		$('.navigation').toggleClass('_active');
+
+		return false;
+	});
+
+	$(document).click(function(event) {
+		if ($(event.target).closest('.navigation').length === 0) {
+			$('.navigation').removeClass('_active');
+			$('.burger-btn').removeClass('_active');
+		}
+	});
+
+	// Счетчик количества товара в форме
+
+	function getChar(event) {
+		if (event.which == null) {
+			if (event.keyCode < 32) return null;
+				return String.fromCharCode(event.keyCode) // IE
+			}
+			if (event.which != 0 && event.charCode != 0) {
+				if (event.which < 32) return null;
+					return String.fromCharCode(event.which) // остальные
+				}
+				return null; // специальная клавиша
+			}
+
+	$('[name=counter]').keypress(function(e) {
+		e = e || event;
+
+		if (e.ctrlKey || e.altKey || e.metaKey) return;
+		var chr = getChar(e);
+		// с null надо осторожно в неравенствах, т.к. например null >= '0' => true!
+		// на всякий случай лучше вынести проверку chr == null отдельно
+		if (chr == null) return;
+
+		if (chr < '0' || chr > '9') {
+			return false;
+		}
+	});
+
+	$('[name=counter]').keyup(function(event) {
+		var input = $(this);
+		if (input.val() < 1) {
+			input.val(input.attr('data-min'))
+		} else if (input.val() > 5000) {
+			input.val(input.attr('data-max'))
+		}
+	});
+
+	// Счетчик количества товара в форме
+	$('.counter').each(function() {
+		var item = $(this),
+		minus = item.find('a.counter__minus'),
+		plus = item.find('a.counter__plus'),
+		input = item.find('input');
+
+		minus.click(function() {
+			var curr_val = parseInt(input.val());
+			if (curr_val > parseInt(input.attr('data-min'))) {
+				curr_val--;
+				input.val(curr_val);
+			}
+			return false;
+		});
+
+		plus.click(function() {
+			var curr_val = parseInt(input.val());
+
+			if (curr_val < parseInt(input.attr('data-max'))) {
+				curr_val++;
+				input.val(curr_val);
+			}
+			return false;
+		});
+	});
+});
